2c7049d2bf5a21c4687e33a7273f62aea564ef56
Shandell Martell <shmartell@stu.lltc.edu>
2024-09-12 08:46:50 -0700

    literals

---
+++ index.py
@@ -0,0 +1,35 @@
+
+
+def main():
+    #variables go up here 
+
+    PI = 3.14
+    
+    #string called my namwe
+    my_name = "Shandell"
+
+    telephone_number = 2184079178
+
+    #string called telephon num
+
+    telephone_number_str = "2184079178"
+    #print statements down here
+    print(PI)
+    print("PI is of type", type(PI))
+
+    #print string my_name
+    print(my_name)
+    print("my_name is of type", type(my_name))
+
+    #print num called telephone num
+    print(telephone_number)
+    print("telephone number is of type", type(telephone_number))
+
+    #print string called telephone num
+    print(telephone_number_str)
+    print("telephone_number is of type",type(telephone_number_str))
+
+
+
+if __name__ == "__main__":
+    main()
